057ae17703e75ae1ea0601551b2440937333ed2c
Joblaque <joachim.onipe@gmail.com>
2021-09-01 09:11:23 -0700

    Low level Commit

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,19 @@
+
+/**
+ * main - block main
+ * output Holberton
+ * Return: 0
+ */
+int main(void)
+{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+return (0);
+}
