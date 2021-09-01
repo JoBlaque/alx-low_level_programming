527a636e1a775db81c8b9b716b89a7c95e204601
Joblaque <joachim.onipe@gmail.com>
2021-08-31 19:39:55 -0700

    Betty Commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - entry point
+ * main - Entry point
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (Successfull)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
